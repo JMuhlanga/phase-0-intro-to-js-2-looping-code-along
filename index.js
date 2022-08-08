21fc4df63a04cc6cf58d88b77daaa71715951e1d
Jose Kupeka Muhlanga <jose.muhlanga@student.moringaschool.com>
2022-08-08 22:42:15 +0300

    Attempted on Aug 8

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+function writeCards(names,giftEvent){
+    let cardMess = [];
+    debugger;
+    for(let i=0; i < names.length;i++){
+        cardMess.push(`Thank you, ${names[i]}, for the wonderful ${giftEvent} gift!`);               
+        debugger;
+    }   
+    return cardMess;
+}
+
+function countDown(number){
+    while(number>=0){
+        number--; 
+        console.log(number);
+        
+    }
+    
+    return countDown;
+                
+                
+}
+
